b1cd1031d2e3047b706feba7aac5b058a66771e6
jeonhyeji <6887>
2021-02-27 15:23:04 +0900

    2021.02.27 Sat

--- DFS/사다리타기.py
+++ DFS/사다리타기.py
@@ -1,2 +1,31 @@
+'''
+도착 지점으로부터 왼쪽,오른쪽,위 순으로 탐색하며 재귀
+    왼쪽,오른쪽을 먼저 탐색하는 것이 Point!
+'''
 
-arr = [list(map(int, input().split())) for _ in range(10)]
+import sys
+
+def dfs(h, w):
+    if h == 0:
+        print(w)
+        sys.exit(0)
+    else:
+        for i in range(3):
+            nh = h + dh[i]
+            nw = w + dw[i]
+            if 0<=nh<n and 0<=nw<n and check[nh][nw] == 0 and arr[nh][nw] == 1:
+                check[nh][nw] = 1
+                dfs(nh, nw)
+                check[nh][nw] = 0
+
+
+
+n = 10
+arr = [list(map(int, input().split())) for _ in range(n)]
+check = [[0]*n for _ in range(n)]
+dw = [1,-1,0]
+dh = [0,0,-1]
+for i in range(n):
+    if arr[n-1][i] == 2:
+        check[n-1][i] = 1
+        dfs(n-1, i)
