4c7e80b2b4d42f95c8bb354f20a23baceeba8dc4
kimdh-hi <zbeld123@gmail.com>
2021-10-30 21:21:05 +0900

    2021.10. 30

---
+++ sparta/boj/2108_통계학.py
@@ -0,0 +1,56 @@
+
+def average(nums):
+    return round(sum(nums) / len(nums))
+
+def median(nums):
+    return nums[len(nums) // 2]
+
+def mod(nums):
+    dic = {}
+    for i in range(len(nums)):
+        n = nums[i]
+        if n in dic:
+            dic[n] += 1
+        else:
+            dic[n] = 1
+
+    most = 0
+    eq_list = []
+    for k, v in dic.items():
+        if v > most: # 더 큰 값 찾았을 때
+            most = v
+            eq_list.clear()
+            eq_list.append(k)
+        elif v == most: # 현재 최빈값과 같을 때
+            eq_list.append(k)
+
+
+    eq_list = sorted(eq_list)
+    if len(eq_list) >= 2:
+        return eq_list[1]
+    return eq_list[0]
+
+
+def nums_range(nums):
+    return nums[-1] - nums[0]
+
+
+n = int(input())
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+nums = sorted(nums)
+
+avg = average(nums)
+med = median(nums)
+mod = mod(nums)
+range = nums_range(nums)
+
+print(avg)
+print(med)
+print(mod)
+print(range)
+
+
